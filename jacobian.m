2f23b532fa3b435a3f16d30c3e8ae3e3ae9a23b3
Tom Richter <trichter93@users.noreply.github.com>
2020-01-26 18:49:01 +0100

    Delete jacobian.m

--- jacobian.m
+++
@@ -1,9 +0,0 @@
-function J = jacobian(x,t)
-n = length(x);
-if n == 2
-    J = [exp(-t.*x(2)) -x(1)*t.*exp(-t.*x(2))];
-end
-if n == 4
-    J = [exp(-t.*x(2)) -x(1)*t.*exp(-t.*x(2)) exp(-t.*x(4)) -x(3)*t.*exp(-t.*x(4))];
-end
-end
